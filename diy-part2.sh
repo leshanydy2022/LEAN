390bb74acf5c2303d2a500bb94994f47afea584e
leshanydy2022 <98255172+leshanydy2022@users.noreply.github.com>
2023-04-04 11:42:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,8 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 # Install theme
 git clone https://github.com/leshanydy2022/luci-theme-bootstrap-mod.git package/lean/luci-theme-bootstrap-mod
 # Install app
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+git clone https://github.com/leshanydy2022/luci-app-smartdns.git package/lean/luci-app-smartdns
 git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 # Modify default theme（FROM luci-theme-bootstrap CHANGE TO luci-theme-bootstrap-mod）
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' ./feeds/luci/collections/luci/Makefile
@@ -26,4 +27,5 @@ rm -rf feeds/packages/net/adguardhome
 git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
 # 为smartDNS插件更换最新的版本
 rm -rf feeds/packages/net/smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+git clone https://github.com/leshanydy2022/smartdns.git feeds/packages/net/smartdns
